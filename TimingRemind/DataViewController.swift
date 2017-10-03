3a8fb7fd8be1404f98c2ea44082d6a337a87d7f6
ChanningKuo <492689363@qq.com>
2017-10-03 18:49:18 +0800

    添加初始化项目代码

---
+++ TimingRemind/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  TimingRemind
+//
+//  Created by Channing Kuo on 2017/10/3.
+//  Copyright © 2017年 Channing Kuo. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
